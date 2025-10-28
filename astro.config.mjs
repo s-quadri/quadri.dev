0aa7eb48ac18a50b1f1b02924c6acc119196edd5
Sophia Quadri <sophia@quadri.dev>
2025-10-28 18:39:13 -0400

    changed site ref

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,13 +12,13 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://gianmarcocavallo.com/",
+  site: "https://sophia.quadri.dev",
   integrations: [
     sitemap(),
     robotsTxt({
       sitemap: [
-        "https://gianmarcocavallo.com/sitemap-index.xml",
-        "https://gianmarcocavallo.com/sitemap-0.xml",
+        "https://sophia.quadri.dev/sitemap-index.xml",
+        "https://sophia.quadri.dev/sitemap-0.xml",
       ],
     }),
     solidJs(),
